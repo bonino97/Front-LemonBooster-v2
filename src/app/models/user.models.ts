075c386022012555cfc8710c07a88399799d9346
Juan Cruz <juanbonino97@gmail.com>
2020-10-23 23:56:31 -0300

    Init

---
+++ src/app/models/user.models.ts
@@ -0,0 +1,8 @@
+export class User {
+
+    constructor(
+        public Email: string,
+        public Password: string,
+    ){  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  staticUrl: 'http://'+localStorage.getItem('IpVPS')+':5000/Static/',
+  authUrl: 'http://auth.lemonbooster.com/api'
+};
+
